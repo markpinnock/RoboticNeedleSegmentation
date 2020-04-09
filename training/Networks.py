093c3646d10705d4d05c3d886d4e45ef65d4d9f8
Mark Pinnock <rmappin@ucl.ac.uk>
2020-04-09 14:05:15 +0100

    Changed activation=relu to tf.nn.relu

--- training/Networks.py
+++ training/Networks.py
@@ -4,22 +4,22 @@ import tensorflow.keras as keras
 
 
 def dnResNetBlock(nc, inputlayer):
-    conv1 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(inputlayer)
-    BN1 = keras.layers.BatchNormalization()(conv1)
-    conv2 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(BN1)
-    BN2 = keras.layers.BatchNormalization()(conv2)
+    conv1 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same')(inputlayer)
+    BN1 = tf.nn.relu(keras.layers.BatchNormalization()(conv1))
+    conv2 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same')(BN1)
+    BN2 = tf.nn.relu(keras.layers.BatchNormalization()(conv2))
     pool = keras.layers.MaxPool3D((2, 2, 1), strides=(2, 2, 1), padding='same')(conv2)
     return BN2, pool
 
 
 def upResNetBlock(nc, inputlayer, skip, tconv_strides):
-    tconv = keras.layers.Conv3DTranspose(nc, (3, 3, 3), strides=tconv_strides, padding='same', activation='relu')(inputlayer)
-    BN1 = keras.layers.BatchNormalization()(tconv)
+    tconv = keras.layers.Conv3DTranspose(nc, (3, 3, 3), strides=tconv_strides, padding='same')(inputlayer)
+    BN1 = tf.nn.relu(keras.layers.BatchNormalization()(tconv))
     concat = keras.layers.concatenate([BN1, skip], axis=4)
-    conv1 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(concat)
-    BN2 = keras.layers.BatchNormalization()(conv1)
-    conv2 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(BN2)
-    BN3 = keras.layers.BatchNormalization()(conv2)
+    conv1 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same')(concat)
+    BN2 = tf.nn.relu(keras.layers.BatchNormalization()(conv1))
+    conv2 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same')(BN2)
+    BN3 = tf.nn.relu(keras.layers.BatchNormalization()(conv2))
     return BN3
 
 
@@ -31,8 +31,8 @@ def UNetGen(input_shape, starting_channels):
     skip2, dnres2 = dnResNetBlock(nc * 2, dnres1)
     skip3, dnres3 = dnResNetBlock(nc * 4, dnres2)
     skip4, dnres4 = dnResNetBlock(nc * 8, dnres3)
-    dn5 = keras.layers.Conv3D(nc * 16, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(dnres4)
-    BN = keras.layers.BatchNormalization()(dn5)
+    dn5 = keras.layers.Conv3D(nc * 16, (3, 3, 3), strides=(1, 1, 1), padding='same')(dnres4)
+    BN = tf.nn.relu(keras.layers.BatchNormalization()(dn5))
 
     upres4 = upResNetBlock(nc * 8, BN, skip4, (2, 2, 1))
     upres3 = upResNetBlock(nc * 4, upres4, skip3, (2, 2, 1))
